8a79f2e46d77faf1f48c00f258be636e58b0f354
pavitkalra150 <c0868150@mylambton.ca>
2022-12-06 20:55:48 -0500

    Fragment demo BUTTON REPLACE FRAGMENT

--- app/src/main/java/com/example/fragmentdemo/SecondActivity.java
+++ app/src/main/java/com/example/fragmentdemo/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -16,6 +17,16 @@ public class SecondActivity extends AppCompatActivity {
 
         //DYNAMICALLY ADDING A FRAGMENT TO SECOND ACTIVITY
         FragmentManager fm = getSupportFragmentManager();
-        fm.beginTransaction().add(R.id.container, new MainFragment()).commit();
+        fm.beginTransaction().add(R.id.container, new MainFragment(), "FIRST").commit();
+
+
+
+        findViewById(R.id.btn).setOnClickListener(v -> replaceFragment()
+        );
+    }
+
+
+    private void replaceFragment(){
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new fragment_main2()).addToBackStack(null).commit();
     }
 }
